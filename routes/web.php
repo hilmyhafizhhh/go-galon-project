e3c7b1aac772412acf8684d42d5daa3aa98f9240
MARIANO <marnojuluk@gmail.com>
2025-10-28 11:09:33 +0700

    halalam dashboard pesanan dan chat

--- routes/web.php
+++ routes/web.php
@@ -4,23 +4,34 @@ use App\Http\Controllers\Auth\ProviderController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Dashboard
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Pesanan
+Route::get('/pesanan', function () {
+    return view('pesanan');
+})->middleware(['auth'])->name('pesanan');
+
+// Chat
+Route::get('/chat', function () {
+    return view('chat');
+})->middleware(['auth'])->name('chat');
+
+// Profile routes
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
+// Google Auth
 Route::get('/auth/google/redirect', [ProviderController::class, 'redirect']);
 Route::get('/auth/google/callback', [ProviderController::class, 'callback']);
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
